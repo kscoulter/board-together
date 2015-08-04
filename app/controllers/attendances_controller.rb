760752ff17295576770c66baded213ebe47030cd
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-04 12:12:22 -0400

    I can now update the value of players needed and render the join button based on whether the event has space

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,6 +6,8 @@ class AttendancesController < ActionController::Base
   def create
     @event = Event.find(params[:event_id])
     Attendance.create(event: @event, user: current_user)
+    @players_needed = @event.num_players
+    @event.update(num_players: @players_needed-1) 
 
     redirect_to event_path(@event)
   end
