df621c27a7e9d8bc7efd2e5730fe42a1206c50be
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-04 10:18:10 -0400

    minimal mvp

--- db/migrate/20150803191343_user_event_association.rb
+++ db/migrate/20150803191343_user_event_association.rb
@@ -1,8 +1,8 @@
 class UserEventAssociation < ActiveRecord::Migration
   def change
     create_table :attendances do |t|
-      t.integer :user_id
-      t.integer :event_id
+      t.belongs_to :user
+      t.belongs_to :event
     end
   end
 end
