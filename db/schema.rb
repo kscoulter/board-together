c4e8f317d3e2023480c2bcab2165b0050188ff86
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-06 15:21:33 -0400

    resolving merge schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20150806010714) do
-=======
-ActiveRecord::Schema.define(version: 20150805185037) do
->>>>>>> add-profiles
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
