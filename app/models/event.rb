f20141cf42e2cec6f847eed846bf581e811c12e4
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-08-06 12:15:19 -0400

    added comments. can create and delete

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  has_many :attendances
-  has_many :comments
+  has_many :attendances, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
